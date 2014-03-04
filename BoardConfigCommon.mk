e9916c7d7eb6b73c5e4250f001147ab531cacdf2
Dave Daynard <nardholio@gmail.com>
2014-03-03 19:27:58 -0500

    qcom-common: allow multiple hardware tunables includes
    Change-Id: I95773d0442cc64027d52c037c86675c6c012b2e4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,7 +48,7 @@ BOARD_BATTERY_DEVICE_NAME := "battery"
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/qcom-common
 
-BOARD_HARDWARE_CLASS := hardware/samsung/cmhw
+BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
 
 # Workaround to allow us to have a local libhealthd
 TARGET_PROVIDES_LIBHEALTHD := true
